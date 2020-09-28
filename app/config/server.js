2f0e206601638022b0919101f2eaff2664c18055
Kyle Hoang <kyle.hoang@noria.no>
2020-09-28 13:54:46 +0700

    init commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'eb8e808d14fbb49ff785a4e632cfab46'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b3dfd1d6-058b-4746-b247-cd16a4d6b3a8'
+};
